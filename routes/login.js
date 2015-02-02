69f02c3d79c345d1c3b9230dbcf3ed515d91b205
Haiko Schol <hs@haikoschol.com>
2015-02-02 18:39:03 +0000

    use findOne() instead of find() and nextObject() for the login query

--- routes/login.js
+++ routes/login.js
@@ -4,15 +4,13 @@ var router = express.Router();
 router.post('/', function(req, res) {
   var query = {'email': req.body.email, 'password': req.body.password};
 
-  req.users.find(query, function(err, cursor) {
-    cursor.nextObject(function(err, user) {
-      if (user === null) {
-        res.redirect('/');
-      } else {
-        req.session.email = user.email;
-        res.redirect('/inbox');
-      }
-    });
+  req.users.findOne(query, function(err, user) {
+    if (user === null) {
+      res.redirect('/');
+    } else {
+      req.session.email = user.email;
+      res.redirect('/inbox');
+    }
   });
 });
 
